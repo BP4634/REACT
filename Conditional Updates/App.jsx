0798193caacf523294f5026062aa47561c60a5f7
BP4634 <123105474+BP4634@users.noreply.github.com>
2023-11-09 11:05:35 -0400

    Create App.jsx

---
+++ Conditional Updates/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export function App(props) {
+    
+    const time = new Date()
+    const day = time.toLocaleString("en-us", {weekday: "long"})
+    const morning = time.getHours() >= 6 && time.getHours() <= 12
+    let dayMessage
+    
+    if (day.toLowerCase() === "monday") {
+        dayMessage = `Happy ${day}`
+    } else if (day.toLowerCase() === "tuesday") {
+        dayMessage = `${day}, four days to go`
+    } else if (day.toLowerCase() === "wednesday") {
+        dayMessage = `${day}, half way there`
+    } else if (day.toLowerCase() === "thursday") {
+        dayMessage = `${day}, start planning for the weekend`
+    } else if (day.toLowerCase() === "friday") {
+        dayMessage = "Woo hoo, the weekend is coming!"
+    } else {
+        dayMessage = "Stay calm and keep having fun"
+    }
+    
+    
+  return (
+    <div className='App'>
+      <h1>{dayMessage}</h1>
+      {morning ? <h2>Have you had breakfast yet?</h2> : ''}
+    </div>
+  );
+}
+
+// Log to console
+console.log('Hello console')
