44c02cec68ad9d2dcaecde6f6492976b8698b72e
BP4634 <123105474+BP4634@users.noreply.github.com>
2023-11-08 19:48:13 -0500

    Create index.jsx

---
+++ React Basics/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+import { App } from './App.jsx'
+// import { ListGroup } from './App.jsx'
+// import { Alert } from './App.jsx'
+// import { Button } from './App.jsx'
+
+// let items = [
+//     'New York',
+//     'San Francisco',
+//     'Tokyo',
+//     'London',
+//     'Paris',
+//     ]
+
+//     function handleSelectItem (item) {
+//       console.log(item)
+//     }
+
+    
+
+ReactDOM.createRoot( 
+  document.querySelector('#root')
+).render(
+  <div>
+  
+  <App/>
+   </div>)
