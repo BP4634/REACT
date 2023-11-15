2dc6939a9fd57a339a6b1255ae467865f85c7b8a
BP4634 <123105474+BP4634@users.noreply.github.com>
2023-11-15 18:28:19 -0400

    Create App.jsx

---
+++ Fetching Data/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useState, useEffect } from 'react'
+
+export function App(props) {
+    const [user, setUser] = React.useState([])
+    
+    const fetchData = () => {
+        fetch("https://randomuser.me/api/?results=1")
+            .then(response => response.json())
+            .then(data => setUser(data))
+        
+    }
+    
+    React.useEffect(() => {
+        fetchData()
+    },[])
+    
+  return Object.keys(user).length > 0 ? (
+    <div className='App'>
+      <h1>Data returned</h1>
+      <h2>First Name: {user.results[0].name.first}</h2>
+      <h2>Last Name: {user.results[0].name.last}</h2>
+    </div>
+  ) : (
+      <h1>Data pending...</h1>
+      )
+}
+
+// Log to console
+console.log('Hello console')
