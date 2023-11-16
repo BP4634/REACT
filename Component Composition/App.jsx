30cb5307e2a1e1f4d77cc4bb334404477bb38350
BP4634 <123105474+BP4634@users.noreply.github.com>
2023-11-16 17:29:02 -0400

    Create App.jsx

---
+++ Component Composition/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Button = ({ children, backgroundColor }) => {
+    return <button style={{ backgroundColor }}>{children}</button>
+}
+
+const Alert = ({ children }) => {
+    return (
+        <>
+        <div className="Overlay">
+        {children}
+        </div>
+        </>
+        )
+}
+
+const DeleteButton = () => {
+    return <Button backgroundColort="red">Delete</Button>
+}
+
+export function App(props) {
+  return (
+    <div className='App'>
+      <header>Little Lemon Restaurant</header>
+      <Alert>
+      <h4>Delete Account</h4>
+      <p>
+      Are you sure you want to proceed? You will miss all your delicious recipes!
+      </p>
+      <DeleteButton />
+      </Alert>
+    </div>
+  );
+}
+
+// Log to console
+console.log('Hello console')
